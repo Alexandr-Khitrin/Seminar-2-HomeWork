efd4aebf9caaf5dfe2ef6f8ad1d90885fb00fbbb
Alexandr <hitrin79@gmail.com>
2022-12-06 04:02:52 +0800

    Zadacha 15

---
+++ HomeWork Seminar-2 Zadacha 15/HomeWork Seminar-2 Zadacha 15/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Write("Введите значение: ");
+int number = int.Parse(Console.ReadLine());
+
+switch (number)
+{
+        case 1:
+        Console.WriteLine("Понедельник - будний день!");
+        break;
+        case 2:
+        Console.WriteLine("Вторник - будний день!");
+        break;
+        case 3:
+        Console.WriteLine("Среда - будний день!");
+        break;
+        case 4:
+        Console.WriteLine("Четверг - будний день!");
+        break;
+        case 5:
+        Console.WriteLine("Пятница - будний день!");
+        break;
+        case 6:
+        Console.WriteLine("Суббота - выходной день!");
+        break;
+        case 7:
+        Console.WriteLine("Воскресенье - выходной день!");
+        break;
+    default:
+        Console.WriteLine("В НЕДЕЛЕ ТОЛЬКО 7 ДНЕЙ!!!");
+        break;   
+}
