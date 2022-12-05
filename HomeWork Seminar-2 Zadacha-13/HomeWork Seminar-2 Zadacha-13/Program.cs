0731d76bf88f42acd3f5dd54f874942124930d9a
Alexandr <hitrin79@gmail.com>
2022-12-06 03:47:21 +0800

    Zadacha 13

---
+++ HomeWork Seminar-2 Zadacha-13/HomeWork Seminar-2 Zadacha-13/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите значение: ");
+int number = int.Parse(Console.ReadLine());
+
+string third_number  = number.ToString();
+
+if(number >=100)
+{
+    Console.WriteLine(third_number[2]);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нету");
+}
