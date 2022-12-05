fcc68d96e7e429203e3bea85391dd5f411192801
Alexandr <hitrin79@gmail.com>
2022-12-06 03:09:41 +0800

    Zadacha 10

---
+++ HomeWork Seminar-2 Zadacha 10/HomeWork Seminar-2 Zadacha 10/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите значение: ");
+int number = int.Parse(Console.ReadLine());
+int two_number = number / 10 % 10;
+Console.WriteLine($"Вторая цифра числа {number} - {two_number}");
